87cc369261fb04cb49535acdd6cfee868b33eacf
Drin Bratotini <drinbratotini@mac.mynetworksettings.com>
2025-07-28 17:44:09 -0400

    fill.asm done

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -1,6 +1,7 @@
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
+// File name: projects/4/Fill.asm
 
 // Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
@@ -8,3 +9,54 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+
+
+
+
+@SCREEN
+D=A
+@i
+M=D // M=16384
+
+
+(LOOP)
+@KBD
+D=M
+@MAKEWHITE
+D;JEQ
+
+// MAKE BLACK
+@i
+A=M
+M=-1
+
+@i
+D=M+1
+M=D
+
+@LOOP
+0;JMP
+
+
+
+@MAKEWHITE
+@KBD
+D=M
+
+@LOOP
+0;JGT
+
+
+
+@i
+A=M
+M=0
+
+@i
+D=M-1
+M=D
+
+@LOOP
+0;JMP
+
