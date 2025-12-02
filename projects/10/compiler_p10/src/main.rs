aa68c56bbd33bf432315ec5771c5756facdb5b23
Drin B <drin.bratotini@gmail.com>
2025-12-01 20:26:54 -0500

    analyzer outputs xml

--- projects/10/compiler_p10/src/main.rs
+++ projects/10/compiler_p10/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(unused, dead_code, non_camel_case_types)]
 use regex::Regex;
+
+use std::fs::{self, File, OpenOptions};
 use std::io::{BufWriter, Write};
-use std::path::Path;
 use std::path::PathBuf;
 use std::{env, io};
-use std::{file, fs};
 
 fn main() -> io::Result<()> {
     println!("Hello, world!");
@@ -15,7 +15,11 @@ fn main() -> io::Result<()> {
     let input_path = PathBuf::from(input_name.clone());
     let buffer = fs::read_to_string(input_name)?;
     if input_path.is_file() {
-        let jack_t = jack_tokenizer::new(&buffer);
+        let mut jack_t = jack_tokenizer::new(&buffer);
+        while jack_t.has_more_tokens() {
+            jack_t.advance();
+            jack_t.write_token_file();
+        }
     }
 
     Ok(())
@@ -48,18 +52,32 @@ enum TOKEN_TYPE {
 }
 struct jack_analyzer {}
 
-struct jack_tokenizer<'a> {
+struct jack_tokenizer {
     tokens: Vec<String>,
-    current_token: Option<&'a str>,
+    current_token: Option<String>,
     pos: usize,
+    tokens_file: Option<File>,
 }
 
-impl<'a> jack_tokenizer<'a> {
-    fn new(file: &'a str) -> Self {
+impl jack_tokenizer {
+    fn new(file: &str) -> Self {
         // sanitize lines
 
         let mut tokens: Vec<String> = Vec::new();
 
+        let mut file_tokens = OpenOptions::new()
+            .truncate(true)
+            .write(true)
+            .create(true)
+            .open("tokens.xml")
+            .unwrap();
+
+        let mut j_tokenizer = Self {
+            tokens,
+            current_token: None,
+            pos: 0,
+            tokens_file: Some(file_tokens),
+        };
         let re = Regex::new(r#""[^"]*"|\w+|[^\w\s]"#).unwrap();
 
         for line in file.lines() {
@@ -69,27 +87,31 @@ impl<'a> jack_tokenizer<'a> {
             if !sanitized_line.is_empty() {
                 re.find_iter(sanitized_line).for_each(|x| {
                     let mut token = x.as_str().to_string();
-
-                    println!("current token is : {}", token);
-                    tokens.push(token);
+                    j_tokenizer.tokens.push(token);
                 });
             }
         }
 
-        Self {
-            tokens,
-            current_token: None,
-            pos: 0,
-        }
+        j_tokenizer
+    }
+
+    fn write_token_file(&mut self) {
+        let token_type = self.token_type_as_str();
+        let token = self.current_token.as_ref().unwrap();
+        let write_token = format!("<{token_type}> {token} </{token_type}>\n");
+        self.tokens_file
+            .as_mut()
+            .unwrap()
+            .write_all(write_token.as_bytes());
     }
 
     fn has_more_tokens(&self) -> bool {
         self.pos < self.tokens.len()
     }
 
-    fn advance(&'a mut self) {
+    fn advance(&mut self) {
         if self.has_more_tokens() {
-            self.current_token = Some(self.tokens[self.pos].as_ref());
+            self.current_token = Some(self.tokens[self.pos].clone());
             self.pos += 1;
         } else {
             self.current_token = None
@@ -127,9 +149,9 @@ impl<'a> jack_tokenizer<'a> {
             "=", "~",
         ];
 
-        match self.current_token {
-            Some(x) if KEYWORDS.contains(&x) => TOKEN_TYPE::KEYWORD,
-            Some(x) if x.len() == 1 && SYMBOLS.contains(&x) => TOKEN_TYPE::SYMBOL,
+        match &self.current_token {
+            Some(x) if KEYWORDS.contains(&x.as_ref()) => TOKEN_TYPE::KEYWORD,
+            Some(x) if x.len() == 1 && SYMBOLS.contains(&x.as_ref()) => TOKEN_TYPE::SYMBOL,
             Some(x) if x.starts_with('"') && x.ends_with('"') && x.len() >= 2 => {
                 TOKEN_TYPE::STRING_CONST
             }
@@ -152,37 +174,123 @@ impl<'a> jack_tokenizer<'a> {
         }
     }
 
-    fn keyword(&self) -> Option<&str> {
+    fn token_type_as_str(&self) -> &str {
         match self.token_type() {
-            TOKEN_TYPE::KEYWORD => self.current_token,
+            TOKEN_TYPE::IDENTIFIER => "identifier",
+            TOKEN_TYPE::KEYWORD => "keyword",
+            TOKEN_TYPE::STRING_CONST => "stringConstant",
+            TOKEN_TYPE::INT_CONST => "integerConstant",
+            TOKEN_TYPE::SYMBOL => "symbol",
+            TOKEN_TYPE::NONE => "",
+        }
+    }
+
+    fn keyword(&self) -> Option<String> {
+        match self.token_type() {
+            TOKEN_TYPE::KEYWORD => self.current_token.clone(),
             _ => None,
         }
     }
 
-    fn symbol(&self) -> Option<&str> {
+    fn symbol(&self) -> Option<String> {
         match self.token_type() {
-            TOKEN_TYPE::SYMBOL => self.current_token,
+            TOKEN_TYPE::SYMBOL => self.current_token.clone(),
             _ => None,
         }
     }
-    fn identifier(&self) -> Option<&str> {
+    fn identifier(&self) -> Option<String> {
         match self.token_type() {
-            TOKEN_TYPE::IDENTIFIER => self.current_token,
+            TOKEN_TYPE::IDENTIFIER => self.current_token.clone(),
             _ => None,
         }
     }
     fn int_val(&self) -> Option<u32> {
         match self.token_type() {
-            TOKEN_TYPE::INT_CONST => self.current_token.and_then(|x| x.parse::<u32>().ok()),
+            TOKEN_TYPE::INT_CONST => self
+                .current_token
+                .clone()
+                .and_then(|x| x.parse::<u32>().ok()),
             _ => None,
         }
     }
-    fn string_val(&self) -> Option<&str> {
+    fn string_val(&self) -> Option<String> {
         match self.token_type() {
-            TOKEN_TYPE::STRING_CONST => self.current_token,
+            TOKEN_TYPE::STRING_CONST => self.current_token.clone(),
             _ => None,
         }
     }
 }
 
-struct compilation_engine {}
+/*
+CompilationEngine: Effects the actual compilation output. Gets its input from a JackTokenizer and
+emits its parsed structure into an output file/stream. The output is generated by a series of compilexxx () routines,
+one for every syntactic element xxx of the Jack grammar. The contract between these routines is that each compilexxx ()
+routine should read the syntactic construct xxx from the input, advance () the tokenizer exactly beyond xxx, and output the parsing of xxx.
+Thus, compilexxx () may only be called if indeed xxx is the next syntactic element of the input.
+ */
+
+struct compilation_engine {
+    file: Option<BufWriter<File>>,
+}
+
+/*Six nonterminal grammar elements ( subroutineCall, subroutineName, varName, className, type, statement )
+have no corresponding compilexxx methods;
+these terminals are parsed directly, by other parsing methods that handle them; */
+impl compilation_engine {
+    fn new() -> io::Result<Self> {
+        let file = OpenOptions::new()
+            .truncate(true)
+            .write(true)
+            .create(true)
+            .open("output.txt")
+            .unwrap();
+
+        let mut writer = Self {
+            file: Some(BufWriter::new(file)),
+        };
+        Ok(writer)
+    }
+
+    fn compile_class() {
+        unimplemented!()
+    }
+    fn compile_class_var_dec() {
+        unimplemented!()
+    }
+    fn compile_subroutine() {
+        unimplemented!()
+    }
+    fn compiler_parameter_list() {
+        unimplemented!()
+    }
+    fn compile_var_dec() {
+        unimplemented!()
+    }
+    fn compile_statements() {
+        unimplemented!()
+    }
+    fn compile_do() {
+        unimplemented!()
+    }
+    fn compile_let() {
+        unimplemented!()
+    }
+    fn compile_while() {
+        unimplemented!()
+    }
+    fn compile_return() {
+        unimplemented!()
+    }
+    fn compile_if() {
+        unimplemented!()
+    }
+    fn compile_expression() {
+        unimplemented!()
+    }
+    fn compile_term() {
+        unimplemented!()
+    }
+    fn compile_expression_list() {
+        unimplemented!()
+    }
+}
